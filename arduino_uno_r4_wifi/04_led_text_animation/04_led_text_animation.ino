0b1142223d345f8266cc600d80d698a371695db4
0xAKD <helloajaydey@gmail.com>
2025-07-05 11:39:09 +0530

    implimented led text scroll animation in arduino uno r4 wifi

---
+++ arduino_uno_r4_wifi/04_led_text_animation/04_led_text_animation.ino
@@ -0,0 +1,22 @@
+#include "ArduinoGraphics.h"
+#include "Arduino_LED_Matrix.h"
+
+ArduinoLEDMatrix matrix;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  matrix.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  const char text[] = "  hello world";  
+  matrix.textScrollSpeed(50);
+  matrix.textFont(Font_5x7);
+  matrix.beginText(0, 1, 0xFFFFFF);
+  matrix.println(text);
+  matrix.endText(SCROLL_LEFT); 
+
+}
